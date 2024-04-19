2a06560853d0fc8eaa02ed1cc08f91374287fc3a
wired <wired67@outlook.de>
2024-04-19 17:58:56 +0200

    pricing request opt

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -99,7 +99,7 @@ const Dashboard = () => {
 
   return (
     <Sidebar>
-      <div className=" grid grid-cols-2 lg:grid-cols-3 gap-4 w-full mb-12">
+      <div className=" grid grid-cols-2 lg:grid-cols-3 gap-4 w-full mb-12 bg-white">
         <div className="px-6 shadow-sm flex bg-gray-100  flex-col py-4 rounded-lg text-gray-800">
           <div className="text-2xl font-semibold text-gray-900">
             Muhammad Kashif
