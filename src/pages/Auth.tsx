504214483ef44df18e6b0cad4cfa53d796ccadcc
wired <wired67@outlook.de>
2024-04-19 16:03:59 +0200

    pricing request opt

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -102,12 +102,84 @@ const Auth: React.FC<AuthTypes> = (
       </p>
     )
   }
-
-  if (loading) {
+  const Content = () => {
+    if (loading) {
+      return(
+        <LoadingIndicator size={50} loading={loading} />
+      )
+    }
     return(
-      <LoadingIndicator size={50} loading={loading} />
+      <form
+        onSubmit={handleRegister}
+        className="space-y-6"
+        action="#"
+        method="POST"
+      >
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={(e) =>
+                setUserInfo({
+                  ...userInfo,
+                  email: e.target.value,
+                })
+              }
+              autoComplete="email"
+              required
+              className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password
+            </label>
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              onChange={(e) =>
+                setUserInfo({
+                  ...userInfo,
+                  password: e.target.value,
+                })
+              }
+              type="password"
+              autoComplete="current-password"
+              required
+              className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            disabled={loading}
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            {loading ? "Login..." : "Login"}
+          </button>
+        </div>
+      </form>
     )
   }
+
   return (
     <div className="bg-white flex min-h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
       <div className={"min-w-[300px] mx-[30px] lg:w-[300px] sm:w-[200px]"}>
@@ -121,74 +193,9 @@ const Auth: React.FC<AuthTypes> = (
         </div>
 
         <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form
-            onSubmit={handleRegister}
-            className="space-y-6"
-            action="#"
-            method="POST"
-          >
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  onChange={(e) =>
-                    setUserInfo({
-                      ...userInfo,
-                      email: e.target.value,
-                    })
-                  }
-                  autoComplete="email"
-                  required
-                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  onChange={(e) =>
-                    setUserInfo({
-                      ...userInfo,
-                      password: e.target.value,
-                    })
-                  }
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                disabled={loading}
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                {loading ? "Login..." : "Login"}
-              </button>
-            </div>
-          </form>
+          {
+            Content()
+          }
           {
             footerText()
           }
