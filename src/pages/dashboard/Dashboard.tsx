89c70d36f607b929069f9da7e92ed01a97e0ab17
wired <wired67@outlook.de>
2024-04-19 17:46:30 +0200

    pricing request opt

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -87,21 +87,26 @@ const Dashboard = () => {
     })
   }, []);
 
-
-  if ( loading ) {
-    return(
-      <div className={"w-full h-full position-absolute justify-center items-center opacity-40"}>
-        <LoadingIndicator size={50} color={"white"} loading={loading} />
-      </div>
-    )
+  const loadingContent = () => {
+    if ( loading ) {
+      return(
+        <div className={"w-[100%] h-[100%] flex flex-1 flex-grow position-absolute justify-center items-center opacity-40 bg-gray-100"}>
+          <LoadingIndicator size={50} color={"white"} loading={loading} />
+        </div>
+      )
+    }
   }
+
   return (
     <Sidebar>
-      <div className="  grid grid-cols-2 lg:grid-cols-3 gap-4 w-full  mb-12">
+      <div className=" grid grid-cols-2 lg:grid-cols-3 gap-4 w-full mb-12">
         <div className="px-6 shadow-sm flex bg-gray-100  flex-col py-4 rounded-lg text-gray-800">
           <div className="text-2xl font-semibold text-gray-900">
             Muhammad Kashif
           </div>
+          {
+            loadingContent()
+          }
           <div className="text-sm">kashif@mail.com</div>
           <div className="text-sm">+92 3095203945</div>
           <div className="text-sm">Alia Town Baghbanpura Lahore Pakistan</div>
