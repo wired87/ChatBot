c0a71d3c0878a2edf3d0848674fd20165a3335e6
wired <wired67@outlook.de>
2024-04-22 16:02:30 +0200

    ad bot modal logic

--- src/components/AddBot.tsx
+++ src/components/AddBot.tsx
@@ -210,6 +210,7 @@ const AddBot: React.FC<Props> = (
         </div>
         <div className="mt-5 sm:mt-6">
           <button
+            type="button"
             className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3
             py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500
             focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
@@ -229,7 +230,7 @@ const AddBot: React.FC<Props> = (
 
   return (
     <Transition.Root show={open} as={Fragment} >
-      <Dialog as="div" className="relative z-50" onClose={updateOpen}>
+      <Dialog as="div" className="relative z-50" onClose={() =>{}}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
