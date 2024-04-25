44c73062dc28d686173293fc7ef23ccb5a2360f9
wired <wired67@outlook.de>
2024-04-25 21:57:47 +0200

    ad bot modal logic

--- src/components/pricing/PriceContainer.tsx
+++ src/components/pricing/PriceContainer.tsx
@@ -183,26 +183,7 @@ const PriceContainer: React.FC<PriceContainerInterface> = (
     return undefined
   }
 
-  const purchaseButton = () => {
-    if ( !(item.title === "Custom") ) {
-      return(
-        <div className="self-stretch relative text-lg-6 leading-[22.31px] font-medium text-dimgray-100 text-center">
-          <button
-            onClick={handlePurchaseSubmit}
-            disabled={buttonDisabled || loading}
-            style={disablesButtonStyle()}
-            className="[border:none] p-0 bg-gray-100 self-stretch rounded-[5.58px] h-[45px] flex flex-row items-center justify-center">
-            <div className="self-stretch flex-1 relative text-mini-9 leading-[22.31px] font-medium font-work-sans text-operator-message-text text-center flex items-center justify-center">
-              {
-                getPurchaseButtonText()
-              }
-            </div>
-            <LoadingIndicator loading={loading} />
-          </button>
-        </div>
-      )
-    }
-  }
+
 
 
 
@@ -220,9 +201,21 @@ const PriceContainer: React.FC<PriceContainerInterface> = (
           priceSection()
         }
       </>
-        {
-          purchaseButton()
-        }
+      <div className="self-stretch relative text-lg-6 leading-[22.31px] font-medium text-dimgray-100 text-center">
+        {!(item.title === "Custom") && (<button
+          onClick={handlePurchaseSubmit}
+          disabled={buttonDisabled || loading}
+          style={disablesButtonStyle()}
+          className="[border:none] p-0 bg-gray-100 self-stretch rounded-[5.58px] h-[45px] flex flex-row items-center justify-center">
+          <div className="self-stretch flex-1 relative text-mini-9 leading-[22.31px] font-medium font-work-sans text-operator-message-text text-center flex items-center justify-center">
+            {
+              getPurchaseButtonText()
+            }
+          </div>
+          <LoadingIndicator loading={loading} />
+        </button>
+        )}
+      </div>
 
       {
         listElements ?
